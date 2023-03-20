e33f22118b7934ab234cb244e61ec1b53b718fbb
Jonathan <jonathan@datastrategypro.com>
2023-03-20 13:33:20 +1300

    ml feature prep

--- functions.R
+++ functions.R
@@ -714,3 +714,30 @@ display_detail_summary <- function(result, ...){
   
   return(rt)
 }
+
+
+
+# machine learning --------------------------------------------------------
+
+make_features <- function(df){
+  df_all <- df %>% 
+    select(-starts_with('result_')) %>% 
+    mutate_all(list(isna = is.na)) %>% 
+    mutate_if(is.character, list(str_len, str_length)) %>% 
+    mutate_if(is.numeric, list(numeric_category = ~case_when(
+      . == 0 ~ 'zero',
+      . > 0 & . < 1 ~ 'small positive',
+      . < 0 & . > -1 ~ 'small negative',
+      . > 1 ~ 'positive',
+      . < -1 ~ 'negative',
+      TRUE ~ 'other'
+    )))
+  
+  l <- df_all %>% 
+    map(~length(unique(.)))
+  
+  cols <- names(l[l > 1 & l < 50])
+  
+  df_subset <- df_all %>% 
+    select(cols)
+}
